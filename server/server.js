955c6c9189b5a67a3c20290c080f259ec2dc8abf
Jose Plasencia <josejpd3@gmail.com>
2023-03-07 08:12:43 -0500

    Require connection

--- server/server.js
+++ server/server.js
@@ -3,3 +3,4 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
