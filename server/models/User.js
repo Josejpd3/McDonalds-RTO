0cdf8127c14e70137b6ba775c2db7d1dc4669522
Jose Plasencia <josejpd3@gmail.com>
2023-03-12 11:57:50 -0400

    Add userSchema username (server/models/User.js)

--- server/models/User.js
+++ server/models/User.js
@@ -2,5 +2,11 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
 
 });
