51d390104df86d1145c0ada835ae7668fb5e7b2d
Jose Plasencia <josejpd3@gmail.com>
2023-03-12 11:59:25 -0400

    Add bcrypt hash password (server/models/User.js)

--- server/models/User.js
+++ server/models/User.js
@@ -30,5 +30,13 @@ const userSchema = new Schema({
       ref: 'Request',
     },
   ],
+});
 
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
 });
