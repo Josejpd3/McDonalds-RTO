ab987c5124393cb50d885ee2a4bfc345c88cf1c9
Jose Plasencia <josejpd3@gmail.com>
2023-03-13 09:03:47 -0400

    Add requestSchema comments (server/models/Request.js)

--- server/models/Request.js
+++ server/models/Request.js
@@ -23,5 +23,24 @@ const requestSchema = new Schema({
     type: String,
     default: "pending"
   },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      commentAuthor: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
 
 });
