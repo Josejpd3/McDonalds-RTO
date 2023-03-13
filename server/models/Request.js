9368670da59645cceed64d112a7b67e4d517241f
Jose Plasencia <josejpd3@gmail.com>
2023-03-13 09:03:04 -0400

    Add requestSchema requestText (server/models/Request.js)

--- server/models/Request.js
+++ server/models/Request.js
@@ -2,5 +2,12 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const requestSchema = new Schema({
+  requestText: {
+    type: String,
+    required: 'You need to leave a request!',
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
 
 });
