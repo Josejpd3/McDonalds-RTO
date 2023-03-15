a5f8a0d7d68bed22d3c3183b03f9022ab4188f03
Jose Plasencia <josejpd3@gmail.com>
2023-03-14 22:37:16 -0400

    Add type Mutation login(server/schemas/typeDefs.js)

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,5 +41,6 @@ const typeDefs = gql`
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!, role: String!): Auth
+    login(email: String!, password: String!): Auth
   }
 `;
