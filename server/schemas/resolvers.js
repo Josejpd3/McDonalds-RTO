5db2a38924c617e8a6cadb11d3d0384aecb2f3a2
Jose Plasencia <josejpd3@gmail.com>
2023-03-16 23:06:24 -0400

    declare resolvers (server/schemas/resolvers.js)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,8 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Request } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+};
+
