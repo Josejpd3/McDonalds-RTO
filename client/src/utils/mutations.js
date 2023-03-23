458cbbfc4534e909e79134efdb7058b4f94c9edc
Jose Plasencia <josejpd3@gmail.com>
2023-03-23 19:52:30 -0400

    Add Add_Comment mutation (client/src/utils/mutations.js)

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -40,3 +40,18 @@ export const ADD_REQUEST = gql`
   }
 `;
 
+export const ADD_COMMENT = gql`
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
