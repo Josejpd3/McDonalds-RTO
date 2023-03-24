7a98d2c7a08bdcc72285f3a28becb58b004caf87
Jose Plasencia <josejpd3@gmail.com>
2023-03-23 20:06:57 -0400

    Add QUERY_USER (client/src/utils/queries.js)

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,19 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      role
+      requests {
+        _id
+        requestText
+        requestStatus
+        createdAt
+      }
+    }
+  }
+`;
+
