d7971de831ff535ae8a3db0603cad7595eb58f99
Jose Plasencia <josejpd3@gmail.com>
2023-03-24 23:32:02 -0400

    Add submit form (client/src/pages/Login.js)

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -19,6 +19,19 @@ const Login = (props) => {
     });
   };
 
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
 
 };
 
