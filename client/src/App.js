f08e751a6c9449ee76db8e32676bff18a2357f39
Jose Plasencia <josejpd3@gmail.com>
2023-03-27 23:08:43 -0400

    Import RRD (client/src/App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
