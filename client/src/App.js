e6b5ef33860dd44a153cd9d114937e361228d015
Jose Plasencia <josejpd3@gmail.com>
2023-03-27 23:08:52 -0400

    Import home (client/src/App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
