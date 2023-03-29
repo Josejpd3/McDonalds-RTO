8c02575fb7b98a1e3245646982b4609e637fe2af
Jose Plasencia <josejpd3@gmail.com>
2023-03-28 22:36:24 -0400

    Add container div (client/src/App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -38,8 +38,10 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="main">
+        <div>
+          <div className="container">
 
+          </div>
         </div>
       </Router>
     </ApolloProvider>
