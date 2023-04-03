eb69f16c1f45edabc2812d83cbd29cd13ccab56d
Jose Plasencia <josejpd3@gmail.com>
2023-04-02 22:44:49 -0400

    Import QUERY_REQUESTS (client/src/pages/Home.js)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,3 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 
 import RequestList from '../components/RequestList';
+
+import { QUERY_REQUESTS } from '../utils/queries';
+
+
