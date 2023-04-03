d23d474e822b9c651cc2bf7c1757571bb129d175
Jose Plasencia <josejpd3@gmail.com>
2023-04-03 09:15:05 -0400

    Add check for data (client/src/pages/Signup.js)

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -45,6 +45,12 @@ const Signup = () => {
         <div className="card">
           <h4 className="card-header">Sign Up</h4>
           <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
               <form onSubmit={handleFormSubmit}>
                 <input
                   className="form-input"
