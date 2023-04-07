8c4baa6fc3eea2b1839cbbaeea4d2db9c5db469e
Jose Plasencia <josejpd3@gmail.com>
2023-04-07 16:40:26 -0400

    Import SingleRequest page (client/src/App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import SingleRequest from './pages/SingleRequest';
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
