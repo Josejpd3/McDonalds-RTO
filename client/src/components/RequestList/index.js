e2730f2dc9026c86d39af60319957ff294f95c36
Jose Plasencia <josejpd3@gmail.com>
2023-04-07 17:18:00 -0400

    Create RequestList component (client/src/components/RequestList.js)

--- client/src/components/RequestList/index.js
+++ client/src/components/RequestList/index.js
@@ -6,3 +6,11 @@ import { useState } from "react";
 import { useMutation } from '@apollo/client';
 import { REMOVE_REQUEST } from "../../utils/mutations";
 
+const RequestList = ({
+  requests,
+  title,
+  showTitle = true,
+  showUsername = true,
+}) => {
+
+};
