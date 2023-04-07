74bf37578d314a1abb6ac001d52c43c76187de3a
Jose Plasencia <josejpd3@gmail.com>
2023-04-07 16:39:44 -0400

    Import Signup page (client/src/App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Signup from './pages/Signup';
 import Login from './pages/Login';
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
