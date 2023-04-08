23416d8fa952a077c25cef37b53c4e997b9ef02c
Jose Plasencia <josejpd3@gmail.com>
2023-04-08 10:29:23 -0400

    Import QUERY_REQUESTS and QUERY_ME (client/src/components/RequestForm.js)

--- client/src/components/RequestForm/index.js
+++ client/src/components/RequestForm/index.js
@@ -5,3 +5,5 @@ import { format, parse } from 'date-fns';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 
+import { ADD_REQUEST } from '../../utils/mutations';
+import { QUERY_REQUESTS, QUERY_ME } from '../../utils/queries';
