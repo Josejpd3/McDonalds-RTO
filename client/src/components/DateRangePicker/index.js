222aff73098bfef374ffb7302aefd9c57dd77c6d
Jose Plasencia <josejpd3@gmail.com>
2023-05-02 23:47:11 -0400

    Add useMutation(ADD_REQUEST) function (client/src/components/DateRangePicker/index.js)

--- client/src/components/DateRangePicker/index.js
+++ client/src/components/DateRangePicker/index.js
@@ -8,6 +8,26 @@ import { QUERY_REQUESTS, QUERY_ME } from '../../utils/queries';
 import Auth from '../../utils/auth';
 
 function DateRangePicker({closeModal}) {
+  const [addRequest] = useMutation(ADD_REQUEST, {
+    update(cache, { data: { addRequest } }) {
+      try {
+        const { requests } = cache.readQuery({ query: QUERY_REQUESTS });
 
-}
+        cache.writeQuery({
+          query: QUERY_REQUESTS,
+          data: { requests: [addRequest, ...requests] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      // update me object's cache
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, requests: [...me.requests, addRequest] } },
+      });
+    },
+  });
+
 
