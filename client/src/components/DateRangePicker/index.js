dff7f6a2f7625dff6f0e6b090ae2e55fa40ea951
Jose Plasencia <josejpd3@gmail.com>
2023-05-04 00:00:00 -0400

    Add calendar set isSingleDate value (client/src/components/DateRangePicker/index.js)

--- client/src/components/DateRangePicker/index.js
+++ client/src/components/DateRangePicker/index.js
@@ -180,6 +180,7 @@ function DateRangePicker({closeModal}) {
                 const isStartSelected = currentStartDate && date.toLocaleDateString() === currentStartDate.toLocaleDateString();
                 const isEndSelected = currentEndDate && date.toLocaleDateString() === currentEndDate.toLocaleDateString();
                 const isSelected = isStartSelected || isEndSelected;
+                const isSingleDate = (isStartSelected && !isEndSelected) || (!isStartSelected && isEndSelected);
             </div>
           ))}
         </div>
