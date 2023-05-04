5de0c90eb1ee3caeecead61da06c8d20a8cdbe59
Jose Plasencia <josejpd3@gmail.com>
2023-05-04 00:08:05 -0400

    Add date-range-picker confirm-container startDate (client/src/components/DateRangePicker/index.js)

--- client/src/components/DateRangePicker/index.js
+++ client/src/components/DateRangePicker/index.js
@@ -209,7 +209,7 @@ function DateRangePicker({closeModal}) {
   return (
     <div className="date-range-picker">
       <div className='confirm-container'>
-
+        <div className={`confirm-date ${(selectedDates.startDate ? `selected` : ``)}`}>Start Date <br/>{(selectedDates.startDate ? `${mainDates.startDate}`: '')}</div>
       </div>
     </div>
   );
