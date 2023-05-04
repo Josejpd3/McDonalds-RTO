e62ddf07b793c16082df67348158d4791a3e01f0
Jose Plasencia <josejpd3@gmail.com>
2023-05-03 23:49:17 -0400

    Add renderCalendar() set calendarDays empty array (client/src/components/DateRangePicker/index.js)

--- client/src/components/DateRangePicker/index.js
+++ client/src/components/DateRangePicker/index.js
@@ -134,5 +134,6 @@ function DateRangePicker({closeModal}) {
     const monthDays = getDaysInMonth(month.getFullYear(), month.getMonth());
     const monthStartWeekday = new Date(month.getFullYear(), month.getMonth(), 1).getDay();
     const monthEndWeekday = new Date(month.getFullYear(), month.getMonth(), monthDays).getDay();
+    const calendarDays = [];
 
   };
