bc5ecaaa8be0dfd57ece2f4f60f0b548fe586da4
Jose Plasencia <josejpd3@gmail.com>
2023-05-05 10:37:18 -0400

    Export BlockedDate (server/models/index.js)

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,4 @@ const User = require('./User');
 const Request = require('./Request');
 const BlockedDate = require('./BlockedDate')
 
-module.exports = { User, Request };
+module.exports = { User, Request, BlockedDate };
