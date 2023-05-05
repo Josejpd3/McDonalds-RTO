e5ad721ab19af08960b4eea640ad77689c711dec
Jose Plasencia <josejpd3@gmail.com>
2023-05-05 10:36:36 -0400

    Import BlockedDate model(server/schemas/resolvers.js)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Request } = require('../models');
+const { User, Request, BlockedDate } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
