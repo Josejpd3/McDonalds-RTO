26ebccbcd61d3600c45c41cebed02a7445f3e18b
Jose Plasencia <josejpd3@gmail.com>
2023-05-05 10:31:58 -0400

    Add date field (server/models/BlockedDate.js)

--- server/models/BlockedDate.js
+++ server/models/BlockedDate.js
@@ -1,5 +1,15 @@
 const { Schema, model } = require('mongoose');
 
 const blockedDateSchema = new Schema({
-
+  date: {
+    type: Date,
+    get: function (timestamp) {
+      const date = new Date(timestamp);
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const year = date.getFullYear().toString().slice(-2);
+      return `${month}/${day}/${year}`;
+    },
+    required: "You need to choose a date",
+  }
 });
