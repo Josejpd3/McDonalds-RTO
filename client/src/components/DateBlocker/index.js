ebab7ca30c936c994ee8436a602d0ad78026aefa
Jose Plasencia <josejpd3@gmail.com>
2023-05-06 10:00:43 -0400

    Import QUERY_BLOCKED_DATES quiery (client/src/components/DateBlocker/index.js)

--- client/src/components/DateBlocker/index.js
+++ client/src/components/DateBlocker/index.js
@@ -3,3 +3,4 @@ import React, { useState } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 
 import { BLOCK_DATE, REMOVE_BLOCKED_DATE} from '../../utils/mutations';
+import { QUERY_BLOCKED_DATES } from '../../utils/queries';
