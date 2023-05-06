6d7e0af9c3f670df8840eeaec7a9c1c0bfa829d8
Jose Plasencia <josejpd3@gmail.com>
2023-05-06 10:01:07 -0400

    Add DateBlocker component (client/src/components/DateBlocker/index.js)

--- client/src/components/DateBlocker/index.js
+++ client/src/components/DateBlocker/index.js
@@ -4,3 +4,7 @@ import { useMutation, useQuery } from '@apollo/client';
 
 import { BLOCK_DATE, REMOVE_BLOCKED_DATE} from '../../utils/mutations';
 import { QUERY_BLOCKED_DATES } from '../../utils/queries';
+
+function DateBlocker() {
+
+  }
