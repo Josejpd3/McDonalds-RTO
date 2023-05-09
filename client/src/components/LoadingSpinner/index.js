2f3e94ba2e44fba2d26419f23f3858d59e02e061
Jose Plasencia <josejpd3@gmail.com>
2023-05-09 15:43:57 -0400

    Add loading-spinner div (client/src/components/LoadingSpinner/index.js)

--- client/src/components/LoadingSpinner/index.js
+++ client/src/components/LoadingSpinner/index.js
@@ -3,7 +3,8 @@ import React from "react";
 const LoadingSpinner = () => {
     return (
         <div className="spinner-container">
-
+            <div className="loading-spinner">
+            </div>
         </div>
     )
 }
