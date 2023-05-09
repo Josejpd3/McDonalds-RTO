ae1188bc419189e32dd68742cf7cec499619d758
Jose Plasencia <josejpd3@gmail.com>
2023-05-08 23:56:06 -0400

    Import QUERY_BLOCKED_DATES (client/src/components/DateRangePicker/index.js)

--- client/src/components/DateRangePicker/index.js
+++ client/src/components/DateRangePicker/index.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 
 import { ADD_REQUEST } from '../../utils/mutations';
-import { QUERY_REQUESTS, QUERY_ME } from '../../utils/queries';
+import { QUERY_REQUESTS, QUERY_ME, QUERY_BLOCKED_DATES } from '../../utils/queries';
 
 import Auth from '../../utils/auth';
 
