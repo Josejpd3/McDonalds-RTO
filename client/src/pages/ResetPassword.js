0236ef72c22bfd4f3a4caa3b665cd4ce9ad4a3b9
Jose Plasencia <josejpd3@gmail.com>
2023-05-10 09:59:48 -0400

    Add ResetPassword component (client/src/pages/ResetPassword.js)

--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -1,3 +1,8 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { RESET_PASSWORD } from '../utils/mutations';
+
+
+const ResetPassword = () => {
+
+};
