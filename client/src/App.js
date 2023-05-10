55ea07ef1fd3d3c48a75a25d4a886c0fbf0c878b
Jose Plasencia <josejpd3@gmail.com>
2023-05-10 10:12:01 -0400

    Import ResetPassword (client/src/App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import SingleRequest from './pages/SingleRequest';
+import ResetPassword from './pages/ResetPassword';
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
