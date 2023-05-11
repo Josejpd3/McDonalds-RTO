0258d350e97b03c7d7e25aa84ad58147a1d5ae25
Jose Plasencia <josejpd3@gmail.com>
2023-05-11 08:00:27 -0400

    Add require dotenv (server/config/connection.js)

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mcdonalds-rto',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
 
 module.exports = mongoose.connection;
