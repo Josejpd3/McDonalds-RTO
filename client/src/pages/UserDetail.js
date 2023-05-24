f2ea0223d4b305e94b7c5a59eccde3a17ac2bab1
Jose Plasencia <josejpd3@gmail.com>
2023-05-24 16:07:07 -0400

    Add UserDetail component (client/src/pages/UserDetail.js)

--- client/src/pages/UserDetail.js
+++ client/src/pages/UserDetail.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+const UserDetail = () => {
+
+};
+
