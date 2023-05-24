5ebee17ae5aeb8ee60be40397772d923f29407cc
Jose Plasencia <josejpd3@gmail.com>
2023-05-24 17:42:29 -0400

    Import LoadingSpinner (client/src/pages/UserDetail.js)

--- client/src/pages/UserDetail.js
+++ client/src/pages/UserDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const UserDetail = () => {
   const { userId } = useParams();
