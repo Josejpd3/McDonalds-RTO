8097940d79c8db5de711c4e8c12e13601bbdb019
Jose Plasencia <josejpd3@gmail.com>
2023-05-24 17:40:20 -0400

    Import LoadingSpinner (client/src/pages/Search.js)

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 import { QUERY_USERS, QUERY_ME } from '../utils/queries';
 
