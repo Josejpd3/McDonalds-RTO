1c095d6050c632aba0c416c30642dd045f640f47
Jose Plasencia <josejpd3@gmail.com>
2023-05-24 15:38:18 -0400

    Add userContainer map through users(client/src/pages/Search.js)

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -38,7 +38,9 @@ const Search = () => {
         </div>
       ) : (
         <div className="userContainer">
+          {users.map((user) => (
 
+          ))}
         </div>
       )}
     </div>
