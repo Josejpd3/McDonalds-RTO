ec0100873151ae566403bd7ea67211e3a4d85ab1
Jose Plasencia <josejpd3@gmail.com>
2023-05-27 09:46:56 -0400

    Update denied response content (client/src/pages/Search.js)

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -26,7 +26,10 @@ const Search = () => {
 
   // Check if the current user has an admin role
   if (currentUser.role !== 'admin') {
-    return <div>Access denied. Only admins can view this page.</div>;
+    return <div className='access-denied'>
+              <a className='go-back' href='/'>&#10132;</a>
+              <h3>Access Denied<br/> Only admins can view this page.</h3>
+            </div>;
   }
 
 
